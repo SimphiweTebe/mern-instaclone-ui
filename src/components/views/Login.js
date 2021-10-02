e1af0eb4865a04a322d3337e32c9abc5e439dbdc
Simphiwe Tebe <simphiwetebe@gmail.com>
2021-10-02 11:16:34 +0200

    Completed user resgister and login forms

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 function Login() {
+
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+    const [errors, setErrors] = useState('')
+
+    const history = useHistory()
+
+    const handleInputs = e => {
+        setUser({...user, [e.target.name]:e.target.value})
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        
+        try{
+            const response = await axios.post('/login', user)
+            history.push('/')
+            console.log(response.data);
+            
+        }catch(res){
+            await setErrors(res.response.data.error)
+        }
+    }  
+
     return (
         <section className="form-wrapper">
-            <h1>Login</h1>
-            <form className="form">
+            <h1>Login to your account</h1>
+            <form className="form" onSubmit={handleSubmit}>
                 <label htmlFor="email">
-                    <input type="email" name="email" id="email" placeholder="Your email" />
+                    <input type="email" name="email" id="email" placeholder="Your email" value={user.email} onChange={handleInputs}/>
                 </label>
                 <label htmlFor="password">
-                    <input type="password" name="password" id="password" placeholder="Password" />
+                    <input type="password" name="password" id="password" placeholder="Password" value={user.password} onChange={handleInputs} />
                 </label>
                 <button>Sign In</button>
+                {
+                    errors !== '' && <p className="errors">{errors}</p>
+                }
                 <h5>
                     <Link to="/signup">Don't have an account? Signup here</Link>
                 </h5>
--- src/components/views/Signup.js
+++ src/components/views/Signup.js
@@ -1,8 +1,10 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 function Signup() {
+
+    const history = useHistory()
 
     const [state, setState] = useState({
         name: '',
@@ -19,8 +21,8 @@ function Signup() {
         e.preventDefault();
         
         try{
-            const req = await axios.post('/signup', state)
-            console.log(req);
+            await axios.post('/signup', state)
+            history.push('/login')
             
         }catch(res){
             await setErrors(res.response.data.error)
@@ -29,13 +31,13 @@ function Signup() {
     
     return (
         <section className="form-wrapper">
-            <h1>Sign up</h1>
+            <h1>Create your new account</h1>
             <form className="form" onSubmit={handleSubmit}>
                 <label htmlFor="name">
-                    <input type="text" name="name" id="name" placeholder="Your username" value={state.name} onChange={handleChange} />
+                    <input type="text" name="name" id="name" placeholder="Username" value={state.name} onChange={handleChange} />
                 </label>
                 <label htmlFor="email">
-                    <input type="email" name="email" id="email" placeholder="Your email" value={state.email} onChange={handleChange} />
+                    <input type="email" name="email" id="email" placeholder="Email" value={state.email} onChange={handleChange} />
                 </label>
                 <label htmlFor="password">
                     <input type="password" name="password" id="password" placeholder="Password" value={state.password} onChange={handleChange} />
